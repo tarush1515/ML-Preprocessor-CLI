26f5b5b164e291fb90606dd68f6c8633b891020b
anushudupi <85610551+anushudupi@users.noreply.github.com>
2022-04-02 03:37:26 +0530

    Update feature.py
    main

--- feature.py
+++ feature.py
@@ -1,11 +1,60 @@
+import os
+from os.path import exists
+import numpy as np
 import pandas as pd
-from PyInquirer import prompt, Separator
-from examples import custom_style_2
-import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, PowerTransformer, RobustScaler, MaxAbsScaler, QuantileTransformer
+import seaborn as sns
+from PyInquirer import prompt, Separator
+from examples import custom_style_2 as style
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, PowerTransformer, RobustScaler, MaxAbsScaler,QuantileTransformer
 
-df = pd.read_csv("train.csv")
+
+def main():
+    flist = [Separator('  csv files present in the current directory =')]
+    for f in os.listdir():
+        if f.endswith('.csv'):
+            flist.append(f)
+    flist.append(Separator(' '))
+    flist.append("enter absolute path")
+    flist.append("exit")
+    ques1 = [
+        {
+            'type': 'list',
+            'qmark': '*',
+            'name': 'user_option',
+            'message': 'Welcome to ML Preprocessor CLI! select csv file:',
+            'choices': flist,
+        }
+    ]
+    ques2 = [
+        {
+            'type': 'input',
+            'qmark': '*',
+            'name': 'file_path',
+            'message': 'enter absolute path of csv file with .csv file extension:',
+        }
+    ]
+    ans1 = prompt(ques1, style=style)
+
+    if ans1.get("user_option") == "exit":
+        exit_code()
+    elif ans1.get("user_option") == "enter absolute path":
+        ans2 = prompt(ques2, style=style)
+        if ans2.get("file_path").endswith('.csv'):
+            file_name = ans2.get("file_path")
+        else:
+            print("\n* Not a csv file -_- ! enter in correct format, Redirecting...\n")
+            main()
+    else:
+        file_name = ans1.get("user_option")
+
+    if exists(file_name):
+        df = pd.read_csv(file_name)
+        df = main_menu(df)
+        save_file(df, file_name)
+    else:
+        print("\n* file doesnt exist in the entered path! Redirecting...\n")
+        main()
 
 
 def main_menu(df):
@@ -14,8 +63,9 @@ def main_menu(df):
             'type': 'list',
             'qmark': '*',
             'name': 'user_option',
-            'message': 'welcome to main menu!! what you wanna do with your data set  - ',
-            'choices': [Separator(' '), Separator('  Data analyser and visualiser options! ='), "print head", "dataframe basic info", "percentage of null values", "dataframe describe",
+            'message': 'Welcome to main menu!! what you wanna do with your data set  - ',
+            'choices': [Separator(' '), Separator('  Data analyser and visualiser options! ='), "print head",
+                        "dataframe basic info", "percentage of null values", "dataframe describe",
                         "dataframe correlation with heatmap", Separator(' '), Separator('  preprocessing options! ='),
                         "nullhandler", "character encoder", "feature scaler",
                         Separator(' '), Separator('  menu options! ='), "discard current work and start new(caution!)",
@@ -49,7 +99,7 @@ def main_menu(df):
             'default': False,
         }
     ]
-    answers1 = prompt(questions1, style=custom_style_2)
+    answers1 = prompt(questions1, style=style)
 
     if answers1.get("user_option") == "print head":
         print(df.head(10))
@@ -78,30 +128,27 @@ def main_menu(df):
         df = feature(df)
         return main_menu(df)
     elif answers1.get("user_option") == "discard current work and start new(caution!)":
-        answers2 = prompt(questions2, style=custom_style_2)
+        answers2 = prompt(questions2, style=style)
         if answers2.get("confirm"):
-            # main()
-            return main_menu(df)  # remove after creating main file
+            main()
         else:
             return main_menu(df)
     elif answers1.get("user_option") == "save preprocessed data!!!":
-        answers3 = prompt(questions3, style=custom_style_2)
+        answers3 = prompt(questions3, style=style)
         if answers3.get("continue"):
             return df
         else:
             return main_menu(df)
     elif answers1.get("user_option") == "Force exit!":
-        answers4 = prompt(questions4, style=custom_style_2)
+        answers4 = prompt(questions4, style=style)
         if answers4.get("exit"):
-            print("Have a good day...\nshutting down...")
-            exit()
+            exit_code()
         else:
             return main_menu(df)
 
 
 def feature(df):
     a = [Separator(' '), Separator(' Select columns for feature scaling =')]
-
 
     flag = 1
     for i in df.columns:
@@ -112,14 +159,14 @@ def feature(df):
             a.append({'name': i})
             flag = 0
     if flag:
-        print("\nevery column of dataframe is object! cant implement feature scaling! Redirecting to main menu...\n")
+        print("\n* every column of dataframe is object! cant implement feature scaling! Redirecting to main menu...\n")
         return df
 
     questions1 = [
         {
             'type': 'checkbox',
             'qmark': '*',
-            'message': 'welcome to feature scaler!\n',
+            'message': 'Welcome to feature scaler! choose columns',
             'name': 'column_option',
             'choices': a
         }
@@ -140,23 +187,23 @@ def feature(df):
             'qmark': '#',
             'name': 'user_option',
             'message': 'confirm! do you want your current chosen feature scaling  options to apply (caution!if you choose "No" data will be unchanged) - ',
-            'choices': [Separator(' '), "Yes and continue", "Yes and return to main menu", Separator(' '), "No and continue",
+            'choices': [Separator(' '), "Yes and continue", "Yes and return to main menu", Separator(' '),
+                        "No and continue",
                         "No and return to main menu"]
         }
     ]
-
-    answers1 = prompt(questions1, style=custom_style_2)
+    answers1 = prompt(questions1, style=style)
     cols = answers1.get('column_option')
     if cols:
         print(cols)
-        answers2 = prompt(questions2, style=custom_style_2)
+        answers2 = prompt(questions2, style=style)
 
         if answers2.get("feature_option") == "back":
             return feature(df)
         elif answers2.get("feature_option") == "main menu":
             return df
         else:
-            answers3 = prompt(questions3, style=custom_style_2)
+            answers3 = prompt(questions3, style=style)
             if answers3.get("user_option") == "Yes and continue":
                 dfx = df[cols]
                 scaler = eval(answers2.get("feature_option"))().fit(dfx)
@@ -176,9 +223,56 @@ def feature(df):
             elif answers3.get("user_option") == "No and return to main menu":
                 return df
     else:
-        print('\nnothing is chosen! Redirecting...\n')
+        print('\n* nothing is chosen! Redirecting...\n')
         return feature(df)
 
 
-df = main_menu(df)
-print(df)
+def save_file(df, file_name):
+    questions1 = [
+        {
+            'type': 'list',
+            'qmark': '*',
+            'name': 'user_option',
+            'message': 'Choose Where you want to save preprocessed data - ',
+            'choices': ["automatic generate (file is generated as initially entered file name extended with '_new')",
+                        "manually enter file name "]
+        }
+    ]
+    questions2 = [
+        {
+            'type': 'input',
+            'qmark': '*',
+            'name': 'file_path',
+            'message': 'enter absolute path of csv file with .csv file extension:',
+        }
+    ]
+    answers1 = prompt(questions1, style=style)
+    if answers1.get(
+            "user_option") == "automatic generate (file is generated as initially entered file name extended with '_new')":
+        df.to_csv(file_name[:-4] + '_new.csv')
+        print("* File successfully saved...\n")
+        exit_code()
+    else:
+        answers2 = prompt(questions2, style=style)
+        if answers2.get("file_path").endswith('.csv'):
+
+            try:
+                df.to_csv(answers2.get("file_path"))
+
+            except:
+                print("\n* absolute path in incorrect format -_- ! Redirecting...\n")
+                save_file(df, file_name)
+            print("* File successfully saved...\n")
+            exit_code()
+        else:
+            print("\n* Not a csv file name -_- ! enter in correct format, Redirecting...\n")
+            save_file(df, file_name)
+
+
+def exit_code():
+    print("Thank you for using ML Preprocessor CLI!, Have a good day...\nshutting down...")
+    exit()
+
+
+if __name__ == '__main__':
+    main()
