5b7d7c7293ffea8ba12c16f9c450274b28f1ab19
linx3273 <ayush96.gupta@gmail.com>
2022-04-12 10:17:58 +0530

    created .gitignore, added a requirements.txt for listing the dependencies for this program, created setup.ps1 to setup all requirements for running the program

---
+++ setup.ps1
@@ -0,0 +1,35 @@
+# This script will create a virtualenv and auto install 
+# all dependencies that are needed for this program
+
+function Green
+{
+    process { Write-Host $_ -ForegroundColor Green }
+}
+
+function Red
+{
+    process { Write-Host $_ -ForegroundColor Red }
+}
+
+
+# opens directory in which the script is located
+cd $PSScriptRoot
+
+Write-Output "Installing virtualenv" | Green
+pip install virtualenv
+
+Write-Output "Creating virtualenv" | Green
+python -m virtualenv .venv
+
+Write-Output "Activating virtualenv" | Green
+.\.venv\Scripts\activate
+
+Write-Output "Installing dependencies" | Green
+pip install -r .\requirements.txt
+
+Write-Output "Creating activate.ps1" | Green
+# to acticvate the virtualenv using autofill instead of entering the directory path given below
+".\.venv\Scripts\activate" > activate.ps1
+# do deactivate the virtualenv just enter deactivate in the terminal
+
+Write-Output "Done" | Green
